0f969ee3124a33a834c8b3e87461789c40efd6c8
soundobj <soundobj@yahoo.com>
2019-02-08 23:40:47 +0100

    initial import

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack v4
+
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports =
+{
+	entry: { main: './src/index.js' },
+	output:
+	{
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js'
+	},
+	target: 'node',
+    externals: [nodeExternals()],
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+
+}
