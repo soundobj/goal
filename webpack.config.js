3d1e747d591e9e7a1e85727bd07a144dfe38219c
soundobj <soundobj@yahoo.com>
2019-02-10 09:48:54 +0100

    postcss config and dist caching

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports =
 {
@@ -9,7 +12,7 @@ module.exports =
 	output:
 	{
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js'
+		filename: '[name].[chunkhash].js',
 	},
 	target: 'node',
     externals: [nodeExternals()],
@@ -22,23 +25,35 @@ module.exports =
                 exclude: /node_modules/,
                 use:
                 {
-                    loader: "babel-loader"
+                    loader: "babel-loader",
                 }
             },
             {
-                test: /\.css$/,
+                test: /\.scss$/,
                 use:
                 [
-                    'style-loader', MiniCssExtractPlugin.loader, 'css-loader'
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
                 ]
             }
         ]
     },
     plugins:
     [
+    	new CleanWebpackPlugin('dist', {}),
         new MiniCssExtractPlugin({
-            filename: 'style.css',
-        })
+            filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new WebpackMd5Hash(),
     ]
 
 }
