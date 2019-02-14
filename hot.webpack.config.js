e7ed04df94eb648edb0e94f00647ee945ea0cbae
soundobj <soundobj@yahoo.com>
2019-02-14 18:31:44 +0100

    add support for raw files in webpack and cclassnames for react

--- hot.webpack.config.js
+++ hot.webpack.config.js
@@ -5,69 +5,72 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const globImporter = require('node-sass-glob-importer');
 module.exports =
-{
-	entry: { main: './src/index.js' },
-	output:
 	{
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-	},
-	target: 'web',
-	devtool: 'eval-source-map',
-    module:
-    {
-        rules:
-        [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:
-                {
-                    loader: "babel-loader",
-                }
-            },
-            {
-        		test: /\.js$/,
-        		use: ["source-map-loader"],
-        		enforce: "pre"
-			},
-            {
-                test: /\.(css|scss)$/,
-                use:
-                [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    // 'sass-loader',
+		entry: { main: './src/index.js' },
+		output:
+		{
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'main.js',
+		},
+		target: 'web',
+		devtool: 'eval-source-map',
+		module:
+		{
+			rules:
+			[
+				{
+					test: /\.txt$/i,
+					use: 'raw-loader',
+				},
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use:
 					{
-						loader: 'sass-loader',
-						options: {
-							importer: globImporter()
-						}
+						loader: "babel-loader",
 					}
-                ]
-            },
-            {
-            	test: /\.svg$/,
-            	use:
-            	{
-            		loader: 'svg-inline-loader',
-            	}
-            }
-        ]
-    },
-    plugins:
-    [
-    	new CleanWebpackPlugin('dist', {}),
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        }),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-    ]
-}
+				},
+				{
+					test: /\.js$/,
+					use: ["source-map-loader"],
+					enforce: "pre"
+				},
+				{
+					test: /\.(css|scss$)/,
+					use:
+					[
+						'style-loader',
+						MiniCssExtractPlugin.loader,
+						'css-loader',
+						'postcss-loader',
+						{
+							loader: 'sass-loader',
+							options: {
+								importer: globImporter()
+							}
+						}
+					]
+				},
+				{
+					test: /\.svg$/,
+					use:
+					{
+						loader: 'svg-inline-loader',
+					}
+				}
+			]
+		},
+		plugins:
+		[
+			new CleanWebpackPlugin('dist', {}),
+			new MiniCssExtractPlugin({
+				filename: 'style.css',
+			}),
+			new HtmlWebpackPlugin({
+				inject: false,
+				hash: true,
+				template: './src/index.html',
+				filename: 'index.html',
+			}),
+		]
+	}
